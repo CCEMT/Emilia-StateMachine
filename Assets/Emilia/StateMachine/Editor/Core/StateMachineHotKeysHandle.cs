d1e34176bbbd8f05248d89d03bb8b12531ade622
EMT <1076995595@qq.com>
2025-04-14 16:40:29 +0800

    更新并增加布局快捷键

---
+++ Assets/Emilia/StateMachine/Editor/Core/StateMachineHotKeysHandle.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Emilia.Node.Editor;
+using Emilia.Node.Universal.Editor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Emilia.StateMachine.Editor
+{
+    public class StateMachineHotKeysHandle: GraphHotKeysHandle<EditorStateMachineAsset>
+    {
+        public override void OnKeyDown(KeyDownEvent evt)
+        {
+            base.OnKeyDown(evt);
+            if (evt.ctrlKey && evt.keyCode == KeyCode.Q)
+            {
+                const float Interval = 50;
+                
+                GraphLayoutUtility.AlignmentType alignmentType = GraphLayoutUtility.AlignmentType.Horizontal | GraphLayoutUtility.AlignmentType.TopOrLeft;
+                GraphLayoutUtility.Start(Interval, alignmentType, smartValue.graphSelected.selected.OfType<IEditorNodeView>().ToList());
+            }
+        }
+    }
+}
