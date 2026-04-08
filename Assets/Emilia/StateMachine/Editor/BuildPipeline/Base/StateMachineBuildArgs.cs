c1f5b2ddb39fee8f3453ff03f6088a7ddc415de7
EMT <1076995595@qq.com>
2026-04-08 20:39:02 +0800

    更新；保存迭代

--- Assets/Emilia/StateMachine/Editor/BuildPipeline/Base/StateMachineBuildArgs.cs
+++ Assets/Emilia/StateMachine/Editor/BuildPipeline/Base/StateMachineBuildArgs.cs
@@ -9,7 +9,6 @@ namespace Emilia.StateMachine.Editor
         public string outputPath;
         
         public bool isGenerateFile;
-        public bool isSaveAsset = true;
         public bool isRefresh = true;
         public bool updateRunner = true;
         public Action generateFileCallback;
@@ -20,7 +19,6 @@ namespace Emilia.StateMachine.Editor
             this.editorAsset = asset;
             this.onBuildComplete = onBuildComplete;
             this.isGenerateFile = true;
-            this.isSaveAsset = true;
             this.isRefresh = true;
         }
     }
